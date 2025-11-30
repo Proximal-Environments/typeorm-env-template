3b6ebc63d1ef618d92d77fcb39e55ae4e88d4235
Rishyanth Kondra <rishyanthkondra@gmail.com>
2025-11-30 18:40:55 +0530

    remove sample, tests

--- test/functional/migrations/show-command/migration/1530542855524-ExampleMigrationTwo.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface } from "../../../../../src/migration/MigrationInterface"
-
-export class ExampleMigrationTwo1530542855524 implements MigrationInterface {
-    public async up() {}
-    public async down() {}
-}
